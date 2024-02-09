83ed82b1ee9eedd1a73c0ab20cce29f39dc85b56
DilanSoto <dilanso12@gmail.com>
2024-02-08 20:11:19 -0400

    Create 0-rectangle.py

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module will provide a class
+that define an empty square
+"""
+
+
+class Rectangle:
+    """
+        An empty class that define a sqr
+    """
+    pass
