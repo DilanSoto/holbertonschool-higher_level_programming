319eea8bc43c6031330145730b55eb23a779973f
DilanSoto <dilanso12@gmail.com>
2024-02-09 10:59:33 -0400

    Create 6-load_from_json_file.py

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Function that creates an Object
+from a JSON file
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """Creates an Object from a JSON file """
+    with open(filename, 'r') as fd:
+        return json.loads(fd.read())
