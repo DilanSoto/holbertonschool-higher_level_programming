2c6176b6d8b1b01e9aa80a9182eececf16f10426
Dilan Soto Ortiz <144055652+DilanSoto@users.noreply.github.com>
2024-03-22 22:48:42 -0400

    Pycode style fix

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -8,16 +8,13 @@ import MySQLdb
 if __name__ == '__main__':
     # Obtaining MySQL credentials from command-line arguments
     user, password, database = argv[1], argv[2], argv[3]
-    
     # Connecting to MySQL database
     db = MySQLdb.connect(host="localhost", user=user,
                          passwd=password, db=database)
-    
     # Executing SQL query to select all states and sorting by id
     db.query("SELECT * FROM states ORDER BY id")
     r = db.store_result()
     t = r.fetch_row(maxrows=0)
-    
     # Printing results
     for i in t:
         print(i)
