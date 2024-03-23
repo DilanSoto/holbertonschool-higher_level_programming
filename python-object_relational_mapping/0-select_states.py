d571b3816fc66581b272f69c9789c33a9bc1b882
Dilan Soto Ortiz <144055652+DilanSoto@users.noreply.github.com>
2024-03-23 12:52:34 -0400

    rename

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,6 +6,19 @@ import MySQLdb as sql
 from sys import argv
 
 if __name__ == '__main__':
+    # Obtaining MySQL credentials from command-line arguments
+    user, password, database = argv[1], argv[2], argv[3]
+    # Connecting to MySQL database
+ # Connecting to MySQL database
+    db = MySQLdb.connect(host="localhost", user=u_name,
+                         passwd=psw, db=base, port=3306)
+    # Executing SQL query to select all states and sorting by id
+    db.query("SELECT * FROM states ORDER BY id")
+    r = db.store_result()
+    t = r.fetch_row(maxrows=0)
+    # Printing results
+    for i in t:
+        print(i)
     db = sql.connect(host="localhost",
                      port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
@@ -13,3 +26,4 @@ if __name__ == '__main__':
     rows = cur.fetchall()
     for row in rows:
         print(row)
+        
