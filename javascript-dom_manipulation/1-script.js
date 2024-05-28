04411b8e2baeb0793b9ae16fc5df0ebb8fc586f5
DilanSoto <dilanso12@gmail.com>
2024-05-28 19:31:38 -0400

    feat: Add JavaScript code to change header color in HTML files

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,5 @@
+const redHeader = document.getElementById('red_header');
+redHeader.addEventListener('click', function() {
+    const header = document.querySelector('header');
+    header.style.color = '#FF0000';
+});
