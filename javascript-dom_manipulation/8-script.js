0d9b473371ca2f6b464b26694d065a027507e97d
DilanSoto <dilanso12@gmail.com>
2024-05-28 19:46:55 -0400

    chore: Update API URL in JavaScript DOM Manipulation script

--- javascript-dom_manipulation/8-script.js
+++ javascript-dom_manipulation/8-script.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const apiUrl = 'https://fourtonfish.com/hellosalut/?lang=fr';
-  
-    fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
-      const helloDiv = document.getElementById('hello');
-      helloDiv.textContent = data.hello;
-    })
-    .catch(error => console.error('Error fetching data:', error));
-  });
+  const apiUrl = 'https://hellosalut.stefanbohacek.dev/?lang=fr';  // Corregido a la URL correcta
+
+  fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => {
+    const helloDiv = document.getElementById('hello');
+    helloDiv.textContent = data.hello;
+  })
+  .catch(error => console.error('Error fetching data:', error));
+});
