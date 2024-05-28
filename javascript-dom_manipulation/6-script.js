e00a67a00f4f125202c0afe716a64e5c941bba26
DilanSoto <dilanso12@gmail.com>
2024-05-28 19:46:46 -0400

    character don't load

--- javascript-dom_manipulation/6-script.js
+++ javascript-dom_manipulation/6-script.js
@@ -1,9 +1,10 @@
-const apiUrl = 'https://swapi-api.hbtn.io/api/films/?format=json';
+const apiUrl = 'https://swapi-api.hbtn.io/api/people/5/?format=json';
 
 fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
         const characterDiv = document.getElementById('character');
-        characterDiv.textContent = data.name;
+        characterDiv.textContent = data.name;  // Asegurándose de que 'name' es la propiedad correcta
     })
     .catch(error => console.error('Error fetching data:', error));
+
